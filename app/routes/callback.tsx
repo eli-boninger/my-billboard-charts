01ed105374bd3b2c0bcd708cb5b3898af54b610a
Eli Boninger <eboninger@gmail.com>
2023-10-12 08:42:35 -0400

    initial api call with spotify token

--- app/routes/callback.tsx
+++ app/routes/callback.tsx
@@ -1,16 +1,16 @@
 import { redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { addUserSpotifyAuth } from "~/models/user.server";
-import { requireUserId } from "~/session.server";
+import { getSession, requireUserId, sessionStorage } from "~/session.server";
 
 export const loader = async ({ request }) => {
   const userId = await requireUserId(request);
+  const session = await getSession(request);
   const url = new URL(request.url);
   const state = url.searchParams.get("state");
   const code = url.searchParams.get("code");
 
   if (state === null) {
-    console.log("redirecting");
     redirect(
       "/#" +
         new URLSearchParams({
@@ -25,22 +25,27 @@ export const loader = async ({ request }) => {
       grant_type: "authorization_code",
     });
 
-    const res = await fetch(
-      `https://accounts.spotify.com/api/token?${params.toString()}`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Basic ${new Buffer.from(
-            `${process.env.SPOTIFY_CLIENT_ID}: ${process.env.SPOTIFY_API_TOKEN}`
-          ).toString("base64")}`,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
+    const res = await fetch(`https://accounts.spotify.com/api/token`, {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${new Buffer.from(
+          `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_API_TOKEN}`
+        ).toString("base64")}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: params,
+    });
+
     if (res.status === 200) {
       const json = await res.json();
-      const user = await addUserSpotifyAuth(userId, json?.refresh_token);
-      throw redirect("/home");
+      addUserSpotifyAuth(userId, json?.refresh_token);
+      session.set("spotify", json?.access_token);
+
+      throw redirect("/home", {
+        headers: {
+          "Set-Cookie": await sessionStorage.commitSession(session),
+        },
+      });
     } else {
       return {};
     }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,27 +1,38 @@
-import { redirect, type V2_MetaFunction } from "@remix-run/node";
-import { Form, Link } from "@remix-run/react";
-
+import { json, redirect, type V2_MetaFunction } from "@remix-run/node";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import crypto from "crypto";
+import { getSession } from "~/session.server";
 import { useOptionalUser } from "~/utils";
 
 export const meta: V2_MetaFunction = () => [{ title: "My Billboard Charts" }];
 
 export const action = () => {
-  const scope = "user-read-private user-read-email";
-
   const params = new URLSearchParams({
     response_type: "code",
     client_id: process.env.SPOTIFY_CLIENT_ID || "",
-    scope: scope,
+    scope: process.env.SPOTIFY_SCOPES || "",
     redirect_uri: process.env.SPOTIFY_AUTH_REDIRECT_URL || "",
-    state: "lkasjdf",
+    state: crypto.randomUUID(),
   });
   return redirect(
     `https://accounts.spotify.com/authorize?${params.toString()}`
   );
 };
 
+export const loader = async ({ request }) => {
+  const session = await getSession(request);
+  const res = await fetch("https://api.spotify.com/v1/me/top/artists", {
+    headers: {
+      Authorization: `Bearer ${session.get("spotify")}`,
+    },
+  });
+  const jsonResult = await res.json();
+  return json({ topTracks: jsonResult?.items });
+};
+
 export default function Home() {
   const user = useOptionalUser();
+  const data = useLoaderData<typeof loader>();
   return (
     <>
       <h1>My billboard charts</h1>
@@ -30,6 +41,9 @@ export default function Home() {
           <button type="submit">Authorize spotify</button>
         </Form>
       )}
+      {data.topTracks.map((t) => (
+        <p key={t.id}>{t.name}</p>
+      ))}
     </>
   );
 }
